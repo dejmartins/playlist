35be611059ef2b88d30e772a5d065df3df255998
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-03-22 12:56:29 +0100

    initial commit

---
+++ src/main/java/africa/semicolon/playlist/PlaylistApplication.java
@@ -0,0 +1,13 @@
+package africa.semicolon.playlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/africa/semicolon/playlist/PlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.semicolon.playlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
