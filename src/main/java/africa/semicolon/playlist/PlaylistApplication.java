55231193994707eaabc8f840e52921d06b33da85
Raheem Adebayo <adraheemzy@gmail.com>
2023-03-22 16:55:46 +0100

    security setup

--- src/main/java/africa/semicolon/playlist/PlaylistApplication.java
+++ src/main/java/africa/semicolon/playlist/PlaylistApplication.java
@@ -1,9 +1,14 @@
 package africa.semicolon.playlist;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "Playlist API", version = "v0.0.1-SNAPSHOT")
+)
 public class PlaylistApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/africa/semicolon/playlist/user/data/models/UserEntity.java
@@ -0,0 +1,29 @@
+package africa.semicolon.playlist.user.data.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private final LocalDateTime dateJoined = LocalDateTime.now();
+
+}
