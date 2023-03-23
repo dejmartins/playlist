0239a1f019aca153ea96c012d23668f2870f20f8
Raheem Adebayo <adraheemzy@gmail.com>
2023-03-23 01:13:35 +0100

    tests for authService

--- src/main/java/africa/semicolon/playlist/auth/dtos/requests/LoginRequestDto.java
+++ src/main/java/africa/semicolon/playlist/auth/dtos/requests/LoginRequestDto.java
@@ -2,9 +2,15 @@ package africa.semicolon.playlist.auth.dtos.requests;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDto {
 
     @NotBlank(message = "Email is required")
