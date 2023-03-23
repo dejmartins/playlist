9f323f588f2d155741fb4790aeadd2a84e4219e6
RevEmmanuel <adeolaae1@gmail.com>
2023-03-23 03:43:56 +0100

    Song and Spotify API

--- src/main/java/africa/semicolon/playlist/PlaylistApplication.java
+++ src/main/java/africa/semicolon/playlist/PlaylistApplication.java
@@ -1,26 +1,21 @@
 package africa.semicolon.playlist;
 
-<<<<<<< Updated upstream
+//<<<<<<< Updated upstream
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import africa.semicolon.playlist.configExtra.RestTemplateConfig;
+import org.springframework.context.annotation.Import;
 
+
+@Import(RestTemplateConfig.class)
 @SpringBootApplication
 @OpenAPIDefinition(
 		info = @Info(title = "Playlist API", version = "v0.0.1-SNAPSHOT")
 )
 @ConfigurationPropertiesScan
-=======
-import africa.semicolon.playlist.configExtra.RestTemplateConfig;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-
-@SpringBootApplication
-@Import(RestTemplateConfig.class)
->>>>>>> Stashed changes
 public class PlaylistApplication {
 
 	public static void main(String[] args) {
