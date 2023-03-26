6e547a66e6eb967d6ccb8c855e03840fcb586772
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-03-26 01:07:40 +0100

    resolve bank account

--- src/main/java/africa/semicolon/playlist/wallet/bankAccountDetails/BankAccountDetailService.java
+++ src/main/java/africa/semicolon/playlist/wallet/bankAccountDetails/BankAccountDetailService.java
@@ -3,7 +3,9 @@ package africa.semicolon.playlist.wallet.bankAccountDetails;
 import africa.semicolon.playlist.wallet.bankAccountDetails.dtos.requests.ResolveAccountRequest;
 import africa.semicolon.playlist.wallet.bankAccountDetails.dtos.responses.ResolveAccountResponse;
 
+import java.io.IOException;
+
 public interface BankAccountDetailService {
 
-    ResolveAccountResponse resolveAccount(ResolveAccountRequest resolveAccountRequest);
+    ResolveAccountResponse resolveAccount(ResolveAccountRequest resolveAccountRequest) throws IOException;
 }
--- src/main/java/africa/semicolon/playlist/wallet/bankAccountDetails/BankAccountDetailServiceImpl.java
+++ src/main/java/africa/semicolon/playlist/wallet/bankAccountDetails/BankAccountDetailServiceImpl.java
@@ -1,14 +1,34 @@
 package africa.semicolon.playlist.wallet.bankAccountDetails;
 
+import africa.semicolon.playlist.config.paystack.PaystackConfiguration;
 import africa.semicolon.playlist.wallet.bankAccountDetails.dtos.requests.ResolveAccountRequest;
 import africa.semicolon.playlist.wallet.bankAccountDetails.dtos.responses.ResolveAccountResponse;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service
+@RequiredArgsConstructor
 public class BankAccountDetailServiceImpl implements BankAccountDetailService{
 
+    private final PaystackConfiguration paystackConfiguration;
+
     @Override
-    public ResolveAccountResponse resolveAccount(ResolveAccountRequest resolveAccountRequest) {
+    public ResolveAccountResponse resolveAccount(ResolveAccountRequest resolveAccountRequest) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.paystack.co/bank/resolve?account_number="
+                        + resolveAccountRequest.getAccountNumber()
+                        + "&bank_code="
+                        + resolveAccountRequest.getBankCode())
+                .get()
+                .addHeader("Authorization", "Bearer " + paystackConfiguration.getSecretKey())
+                .build();
+        Response response = client.newCall(request).execute();
         return null;
     }
 }
