c8af7cd61415c0d63a666d0386567f4603c39d34
Michael Dean <o.michaeldean@gmail.com>
2023-03-27 15:36:36 +0100

    updated

--- src/main/java/africa/semicolon/playlist/config/AppConfig.java
+++ src/main/java/africa/semicolon/playlist/config/AppConfig.java
@@ -3,6 +3,7 @@ package africa.semicolon.playlist.config;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,7 +31,17 @@ public class AppConfig {
     }
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+      ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STANDARD);
+
+      return mapper;
     }
+
+
+
+
 
 }
